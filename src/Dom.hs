a8c1f6fbeb57b6a5520deae03aa983f6baf2b12e
Leif <lgrele@gmail.com>
2014-09-07 14:34:31 -0700

    Partial work on implementing tests.

--- src/Dom.hs
+++ src/Dom.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Dom where
 
 import qualified Data.HashMap.Strict as HM
@@ -27,3 +28,29 @@ text = flip NTree [] . Text
 
 elem :: T.Text -> AttrMap -> [Node] -> Node
 elem name atts cs = NTree (Element (ElementData name atts)) cs
+
+
+-------------------------------- USED FOR TESTS -------------------------------
+
+-- print a node as an html string
+toHtml :: Node -> T.Text
+toHtml (NTree (Text t) cs) = t `T.append` (T.concat $ map toHtml cs)
+toHtml (NTree (Element (ElementData t m)) cs) =
+    tag `T.append` attrs `T.append` ('>' `T.cons` children `T.append` tag')
+  where
+    tag = '<' `T.cons` t `T.snoc` ' '
+    attrs = T.cons ' ' $ T.intercalate " " $ map (\(x,y)->x`T.append`"="`T.append`y) $ HM.toList m
+    children = T.concat $ map toHtml cs
+    tag' = '<' `T.cons` t `T.snoc` '>'
+
+Instance Arbitrary (NTree NodeType) where
+    arbitrary = sized tree'
+      where
+        tree' 0 =
+            oneof [liftM text arbitrary,
+                   liftM3 elem arbitrary arbitrary []
+        tree' n | n > 0 =
+            oneof [liftM text arbitrary,
+                   liftM3 elem arbitrary arbitrary subtree]
+        subtree = tree' (n-1) -- n decides depth of tree
+    coarbitrary = 
