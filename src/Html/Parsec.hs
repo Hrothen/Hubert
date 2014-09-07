87e8dbae2da3d2932afabbdd1eb11519092285e7
Leif <lgrele@gmail.com>
2014-09-07 13:23:25 -0700

    Fixed parsing of leading spaces.
    I hope.

--- src/Html/Parsec.hs
+++ src/Html/Parsec.hs
@@ -17,6 +17,8 @@ parseHtml :: T.Text -> Either ParseError Node
 parseHtml s = case parse parseNodes "" s of
               Left err -> Left err
               Right nodes -> Right $
+                -- FIXME: this is consistant with Robinson's setup, but will skip adding the html element
+                -- if the DOM happens to be a single tree
                 if length nodes == 1
                 then head nodes
                 else Dom.elem "html" HM.empty nodes
@@ -26,17 +28,24 @@ parseNodes = manyTill parseNode eof
 
 parseNode = parseElement <|> parseText
 
-parseText = liftM Dom.text $ manyTill anyChar (try (lookAhead $ char '<'))
+parseText = liftM Dom.text $ many (noneOf "<")
 
 parseElement = do
+    -- opening tag
     (tag, attrs) <- between (char '<') (char '>') tagData
+    -- contents
     children <- parseChildren
+    -- closing tag
     string $ tag ++ ">" -- "</" is consumed by parseChildren, maybe bad form?
     return $ Dom.elem tag attrs children
 
 
-parseChildren = manyTill (spaces >> parseNode) end
-  where end = eof <|> (try (string "</") >> return ())
+parseChildren = spaces >> manyTill parseChild end
+  where
+    end = eof <|> (try (string "</") >> return ())
+
+    parseChild = spacesAfter parseNode
+
 
 tagData = do
     t <- tagName
@@ -46,7 +55,7 @@ tagData = do
 tagName = many1 alphaNum
 
 --this is safe because attribute will fail without consuming on '>''
-attributes = liftM HM.fromList $ many (spaces >> attribute)
+attributes = liftM HM.fromList $ spaces >> many (spacesAfter attribute)
 
 attribute = do
     name <- tagName
@@ -54,3 +63,7 @@ attribute = do
     open <- char '"' <|> char '\''
     value <- many (noneOf [open])
     return (name, value)
+
+
+-- run parser p and then strip the trailing spaces, returning the result of p.
+spacesAfter p = p <* spaces
