b1a0273ba8a6599c3d11e01e342fe138286a73f6
Leif <lgrele@gmail.com>
2014-09-29 21:50:48 -0700

    Added error handling to Layout.hs

--- src/Layout.hs
+++ src/Layout.hs
@@ -3,6 +3,7 @@ module Layout where
 
 import Prelude hiding (lookup)
 import Control.Applicative ((<$>))
+import Control.Monad (foldM)
 import Data.List (foldl', groupBy)
 import Data.Maybe (fromMaybe)
 
@@ -62,111 +63,126 @@ buildLayoutTree root = case display root of
         ns' = map blt $ filter ((/=DisplayNone) . display) cs
 
 
-layout :: LayoutBox -> Dimensions -> LayoutBox
+layout :: LayoutBox -> Dimensions -> Either T.Text LayoutBox
 layout l@(NTree (_,box)_) contBlock = case box of
     BlockNode  _   -> layoutBlock contBlock l
     InlineNode _   -> undefined
     AnonymousBlock -> undefined
 
 
-layoutBlock :: Dimensions -> LayoutBox -> LayoutBox
-layoutBlock dim = calcHeight . layoutChildren
-                . calcPosition dim . calcWidth dim
+layoutBlock :: Dimensions -> LayoutBox -> Either T.Text LayoutBox
+-- layoutBlock dim = calcHeight . layoutChildren
+--                 . calcPosition dim . calcWidth dim
+layoutBlock dim root = calcWidth dim root >>= calcPosition dim
+              >>= layoutChildren >>= calcHeight
 
-calcWidth :: Dimensions -> LayoutBox -> LayoutBox
-calcWidth contBlock root@(NTree (dim,x) y) = let
-    style = getStyledElem root
-    auto = Keyword "auto"
-    zero = Length 0 Px
-    w = fromMaybe auto $ value style "width"
-    vals = map (\a -> lookup style a zero) [
-                  ["margin-left"       , "margin"]
-                , ["margin-right"      , "margin"]
-                , ["border-left-width" , "border-width"]
-                , ["border-right-width", "border-width"]
-                , ["padding-left"      , "padding"]
-                , ["padding-right"     , "padding"] ]
-    total = sum $ map toPx (w:vals)
-    underflow = width contBlock - total
+calcWidth :: Dimensions -> LayoutBox -> Either T.Text LayoutBox
+calcWidth contBlock root@(NTree (dim,x) y) = do
+    style <- getStyledElem root
+    let
+      auto = Keyword "auto"
+      zero = Length 0 Px
+      w = fromMaybe auto $ value style "width"
+      vals = map (\a -> lookup style a zero) [
+                    ["margin-left"       , "margin"]
+                  , ["margin-right"      , "margin"]
+                  , ["border-left-width" , "border-width"]
+                  , ["border-right-width", "border-width"]
+                  , ["padding-left"      , "padding"]
+                  , ["padding-right"     , "padding"] ]
+      total = sum $ map toPx (w:vals)
+      underflow = width contBlock - total
 
-    ([ml'',mr''],vals') = splitAt 2 vals
-    (w',ml',mr') = checkUnderflow w $ checkAutoMargins (ml'',mr'')
+      ([ml'',mr''],vals') = splitAt 2 vals
+      (w',ml',mr') = checkUnderflow w $ checkAutoMargins (ml'',mr'')
 
-    checkAutoMargins (x,y)
-        | w /= auto && total > width contBlock = (check x,check y)
-        | otherwise = (x,y)
-      where check a = if a == auto then zero else a
+      checkAutoMargins (x,y)
+          | w /= auto && total > width contBlock = (check x,check y)
+          | otherwise = (x,y)
+        where check a = if a == auto then zero else a
 
-    checkUnderflow w (mlf,mrt) = case (w == auto, mlf == auto, mrt == auto) of
-        (False,False,False) -> (w , mlf, Length (toPx mrt + underflow) Px)
-        (False,False,True)  -> (w , mlf, Length underflow Px)
-        (False,True,False)  -> (w , Length underflow Px    , mrt)
-        (False,True,True)   -> (w , Length (underflow/2) Px, Length (underflow/2) Px)
-        (True,_,_)          ->
-            let l = if mlf == auto then zero else mlf
-                r = if mrt == auto then zero else mrt
-             in if underflow >= 0  then (Length underflow Px,l,r)
-                                   else (zero,l,Length (toPx r + underflow) Px)
+      checkUnderflow w (mlf,mrt) = case (w == auto, mlf == auto, mrt == auto) of
+          (False,False,False) -> (w , mlf, Length (toPx mrt + underflow) Px)
+          (False,False,True)  -> (w , mlf, Length underflow Px)
+          (False,True,False)  -> (w , Length underflow Px    , mrt)
+          (False,True,True)   -> (w , Length (underflow/2) Px, Length (underflow/2) Px)
+          (True,_,_)          ->
+              let l = if mlf == auto then zero else mlf
+                  r = if mrt == auto then zero else mrt
+               in if underflow >= 0  then (Length underflow Px,l,r)
+                                     else (zero,l,Length (toPx r + underflow) Px)
 
-    [w'',ml,mr,blw,brw,plf,prt] = map toPx (w':ml':mr':vals')
-
-
-    updateDim d = let pad = padding d
-                      mar = margin d
-                      bor = border d
-                   in d{ width = w''
-                       , padding = pad{ left = plf, right = prt}
-                       , border  = bor{ left = blw, right = brw}
-                       , margin  = mar{ left = ml,  right = mr} }
-
-    in NTree (updateDim dim,x) y
+      [w'',ml,mr,blw,brw,plf,prt] = map toPx (w':ml':mr':vals')
 
 
-calcPosition :: Dimensions -> LayoutBox -> LayoutBox
-calcPosition contBlock root@(NTree (dim,a)b) = let
-    style = getStyledElem root
-    zero = Length 0 Px
+      updateDim d = let pad = padding d
+                        mar = margin d
+                        bor = border d
+                     in d{ width = w''
+                         , padding = pad{ left = plf, right = prt}
+                         , border  = bor{ left = blw, right = brw}
+                         , margin  = mar{ left = ml,  right = mr} }
 
-    vals = map (toPx .  (\a -> lookup style a zero)) [
-                  ["margin-top"         , "margin"]
-                , ["margin-bottom"      , "margin"]
-                , ["border-top-width"   , "border-width"]
-                , ["border-bottom-width", "border-width"]
-                , ["padding-top"        , "padding"]
-                , ["padding-bottom"     , "padding"] ]
-
-    updateDim d [mt,mb,bt,bb,pt,pb] =
-        let pad = padding d
-            mar = margin d
-            bor = border d
-            x' = x contBlock
-               + left (margin d)
-               + left (border d)
-               + left (padding d)
-            y' = y contBlock + height contBlock + pt + bt + mt
-         in d{ x = x'
-             , y = y'
-             , padding = pad{ top = pt, bottom = pb }
-             , border  = bor{ top = bt, bottom = bb }
-             , margin  = mar{ top = mt, bottom = mb } }
-
-    in NTree (updateDim dim vals,a) b
+    return $ NTree (updateDim dim,x) y
 
 
-layoutChildren :: LayoutBox -> LayoutBox
-layoutChildren (NTree (dim,x) cs) = NTree (dim',x) cs'
-  where
-    (dim',cs') = foldl' foo (dim,[]) cs
-    foo (d,acc) c@(NTree (cdim,_) _) = let c' = layout c d in
-        (d{height = height d + marginBoxHeight cdim}, acc ++ [c'])
+calcPosition :: Dimensions -> LayoutBox -> Either T.Text LayoutBox
+calcPosition contBlock root@(NTree (dim,a)b) = do
+    style <- getStyledElem root
+    
+    let
+      zero = Length 0 Px
+
+      vals = map (toPx .  (\a -> lookup style a zero)) [
+                    ["margin-top"         , "margin"]
+                  , ["margin-bottom"      , "margin"]
+                  , ["border-top-width"   , "border-width"]
+                  , ["border-bottom-width", "border-width"]
+                  , ["padding-top"        , "padding"]
+                  , ["padding-bottom"     , "padding"] ]
+
+      updateDim d [mt,mb,bt,bb,pt,pb] =
+          let pad = padding d
+              mar = margin d
+              bor = border d
+              x' = x contBlock
+                 + left (margin d)
+                 + left (border d)
+                 + left (padding d)
+              y' = y contBlock + height contBlock + pt + bt + mt
+           in d{ x = x'
+               , y = y'
+               , padding = pad{ top = pt, bottom = pb }
+               , border  = bor{ top = bt, bottom = bb }
+               , margin  = mar{ top = mt, bottom = mb } }
+
+    return $ NTree (updateDim dim vals,a) b
 
 
-calcHeight :: LayoutBox -> LayoutBox
-calcHeight root@(NTree (d,x)y) = let
-    d' = case value (getStyledElem root) "height" of
-        Just (Length h Px)  -> d{height=h}
-        Nothing             -> d
-    in NTree (d',x) y
+-- layoutChildren :: LayoutBox -> LayoutBox
+-- layoutChildren (NTree (dim,x) cs) = NTree (dim',x) cs'
+--   where
+--     (dim',cs') = foldl' foo (dim,[]) cs
+--     foo (d,acc) c@(NTree (cdim,_) _) = let c' = layout c d in
+--         (d{height = height d + marginBoxHeight cdim}, acc ++ [c'])
+
+layoutChildren (NTree (dim,x) cs) = do
+    (dim',cs') <- foldM foo (dim,[]) cs
+    return $ NTree (dim',x) cs'
+
+    where
+        foo (d,acc) c@(NTree (cdim,_) _) = do
+            c' <- layout c d
+            return (d{height = height d + marginBoxHeight cdim}, acc ++ [c'])
+
+
+calcHeight :: LayoutBox -> Either T.Text LayoutBox
+calcHeight root@(NTree (d,x)y) = do
+    s <- getStyledElem root
+    let d' = case value s "height" of
+             Just (Length h Px)  -> d{height=h}
+             Nothing             -> d
+    return $ NTree (d',x) y
 
 
 marginBoxHeight :: Dimensions -> Float
@@ -175,8 +191,9 @@ marginBoxHeight (Dimensions _ _ _ h p b m) = sum [ h, top p, bottom p
                                                  , top m, bottom m ]
 
 
-getStyledElem :: LayoutBox -> StyledNode
+getStyledElem :: LayoutBox -> Either T.Text StyledNode
 getStyledElem (NTree (_,box) _) = case box of
-    BlockNode  s   -> NTree s []
-    InlineNode s   -> NTree s []
-    AnonymousBlock -> undefined -- this is actually an error
+    BlockNode  s   -> Right $ NTree s []
+    InlineNode s   -> Right $ NTree s []
+    AnonymousBlock -> Left "Error: attempted to access the nonexistant\
+                           \ StyleNode of an AnonymousBlock"
